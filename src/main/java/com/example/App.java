651516abcb9432d0afa025f5a8dd0f30520344e1
Freyathakur <156624157+Freyathakur@users.noreply.github.com>
2026-05-05 07:08:12 +0000

    test: trigger jvm_missing_dependency (missing Guava)

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.google.common.collect.ImmutableList;
+
+public class App {
+    public static void main(String[] args) {
+        ImmutableList<String> items = ImmutableList.of("a", "b", "c");
+        System.out.println(items);
+    }
+}
