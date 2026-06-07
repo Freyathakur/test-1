bfbee5068aa99be3336d9aef0a55328a6d5bc36b
Atharv <atharvs@rbpfinivis.com>
2026-06-07 19:15:16 +0530

    test: restore pyapp add(); introduce goapp Reverse() loop bug for live go dry-run test

--- goapp/strutil/strutil.go
+++ goapp/strutil/strutil.go
@@ -6,7 +6,7 @@ import "strings"
 // Reverse returns the string with its characters in reverse order.
 func Reverse(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(runes)-1; i > j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
